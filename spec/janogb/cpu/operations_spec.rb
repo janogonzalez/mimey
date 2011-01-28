9fb56a8e8e622bffdd10365f9911415e123cdcbf
Alejandro González <info@janogonzalez.com>
2011-01-28 16:05:09 -0300

    More testing

--- spec/janogb/cpu/operations_spec.rb
+++ spec/janogb/cpu/operations_spec.rb
@@ -17,35 +17,63 @@ describe "CPU operations" do
     cpu.clock.should == 1
   end
   
-  it "should have LD RR,nn operations that load a 16 bit value into a 16 bit register" do
-    cpu = CPU.new
-    
-    cpu.load_with(0x01, 0xAB, 0xCD).run(1)
-    
-    [:a, :f, :d, :e, :h, :l, :sp].each do |r|
-      cpu.instance_variable_get("@#{r}").should == 0x00
+  describe "LD RR,nn operations" do
+    it "must be 4" do
+      cpu = CPU.new
+      
+      [:ld_bc_nn, :ld_de_nn, :ld_hl_nn, :ld_sp_nn].each do |m|
+        cpu.should respond_to m
+      end
     end
+  
+    it "must load a 16 bit value into a 16 bit register" do
+      cpu = CPU.new
     
-    cpu.bc.should == 0xABCD
-    cpu.pc.should == 0x0003
-    cpu.clock.should == 3
+      cpu.load_with(0x01, 0xAB, 0xCD).run(1)
+    
+      [:a, :f, :d, :e, :h, :l, :sp].each do |r|
+        cpu.instance_variable_get("@#{r}").should == 0x00
+      end
+    
+      cpu.bc.should == 0xABCD
+      cpu.pc.should == 0x0003
+      cpu.clock.should == 3
+    end
   end
   
-  it "should have LD (RR),A operations that load the A register into the memory at address pointed by register RR" do
-    cpu = CPU.new(a:0xAB, b: 0xCA, c:0xFE)
-    
-    cpu.load_with(0x02).run(1)
-    
-    [:f, :d, :e, :h, :l, :sp].each do |r|
-      cpu.instance_variable_get("@#{r}").should == 0x00
+  describe "LD (RR), A operations" do
+    it "must be 3" do
+      cpu = CPU.new
+      
+      [:ld_mbc_a, :ld_mde_a, :ld_mhl_a].each do |m|
+        cpu.should respond_to m
+      end
     end
+  
+    it "must load the A register into the memory at address pointed by register RR" do
+      cpu = CPU.new(a:0xAB, b: 0xCA, c:0xFE)
     
-    cpu.mmu[0xCAFE] = 0xAB
-    cpu.pc.should == 0x0001
-    cpu.clock.should == 2
+      cpu.load_with(0x02).run(1)
+    
+      [:f, :d, :e, :h, :l, :sp].each do |r|
+        cpu.instance_variable_get("@#{r}").should == 0x00
+      end
+    
+      cpu.mmu[0xCAFE] = 0xAB
+      cpu.pc.should == 0x0001
+      cpu.clock.should == 2
+    end
   end
   
   describe "INC RR operations" do
+    it "must be 4" do
+      cpu = CPU.new
+      
+      [:inc_bc, :inc_de, :inc_hl, :inc_sp].each do |m|
+        cpu.should respond_to m
+      end
+    end
+    
     it "should increment a register" do
       cpu = CPU.new
       
@@ -73,4 +101,5 @@ describe "CPU operations" do
       cpu.clock.should == 2
     end
   end
+  
 end
