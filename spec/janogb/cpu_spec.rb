b1daad2c1d5deb9cb312ee09fa3e5998b68ae256
Jano González <info@janogonzalez.com>
2014-02-23 15:25:04 +0100

    refactor CPU spec

--- spec/janogb/cpu_spec.rb
+++ spec/janogb/cpu_spec.rb
@@ -1,181 +1,136 @@
-require 'rspec'
-require 'janogb'
+require 'spec_helper'
 
-describe "CPU" do
-  include JanoGB
+describe JanoGB::CPU do
+  subject(:cpu) { JanoGB::CPU.new(options) }
+  let(:options) { JanoGB::CPU::DEFAULTS }
 
-  describe "A new CPU" do
-    it "should have all registers in 0 if no options where provided" do
-      cpu = CPU.new
+  [:a, :f, :b, :c, :d, :e, :h, :l, :pc, :sp].each do |r|
+    its(r) { should == 0 }
+  end
 
-      [:a, :f, :b, :c, :d, :e, :h, :l, :pc, :sp].each do |r|
-        cpu.instance_variable_get("@#{r}").should == 0x00
-      end
+  context 'when options are provided' do
+    let(:options) {{
+      a: 0x0A,
+      f: 0x0F,
+      b: 0x0B,
+      c: 0x0C,
+      d: 0x0D,
+      e: 0x0E,
+      h: 0x11,
+      l: 0x15,
+      pc: 0xCAFE,
+      sp: 0xFEA0
+    }}
+
+    [:a, :f, :b, :c, :d, :e, :h, :l, :pc, :sp].each do |r|
+      its(r) { should == options[r] }
     end
 
-    it "should initialize all registers with the options values" do
-      options = {
-        a: 0x0A,
-        f: 0x0F,
-        b: 0x0B,
-        c: 0x0C,
-        d: 0x0D,
-        e: 0x0E,
-        h: 0x11,
-        l: 0x15,
-        pc: 0xCAFE,
-        sp: 0xFEA0
-      }
+    its(:af) { should == 0x0A0F }
+    its(:bc) { should == 0x0B0C }
+    its(:de) { should == 0x0D0E }
+    its(:hl) { should == 0x1115 }
+  end
 
-      cpu = CPU.new(options)
+  describe '#af=' do
+    before { cpu.af = 0xABCD }
 
-      [:a, :f, :b, :c, :d, :e, :h, :l, :pc, :sp].each do |r|
-        cpu.instance_variable_get("@#{r}").should == options[r]
-      end
+    its(:a) { should == 0xAB }
+    its(:f) { should == 0xCD }
+  end
+
+  describe '#bc=' do
+    before { cpu.bc = 0xABCD }
+
+    its(:b) { should == 0xAB }
+    its(:c) { should == 0xCD }
+  end
+
+  describe '#de=' do
+    before { cpu.de = 0xABCD }
+
+    its(:d) { should == 0xAB }
+    its(:e) { should == 0xCD }
+  end
+
+  describe '#hl=' do
+    before { cpu.hl = 0xABCD }
+
+    its(:h) { should == 0xAB }
+    its(:l) { should == 0xCD }
+  end
+
+  describe '#z_flag' do
+    context 'when the flag is off' do
+      let(:options) {{ f: 0b0111_0000 }}
+
+      its(:z_flag) { should be_false }
     end
 
-    it "should have its clock in 0" do
-      cpu = CPU.new()
+    context 'when the flag is on' do
+      let(:options) {{ f: 0b1000_0000 }}
 
-      cpu.clock.should == 0
+      its(:z_flag) { should be_true }
     end
   end
 
-  describe "16 bits 'virtual' registers" do
-    it "should get the 'virtual' AF register based on registers A and F" do
-      cpu = CPU.new(a: 0xAB, f: 0xCD)
+  describe '#n_flag' do
+    context 'when the flag is off' do
+      let(:options) {{ f: 0b1011_0000 }}
 
-      cpu.af.should == 0xABCD
+      its(:n_flag) { should be_false }
     end
 
-    it "should set the 'virtual' AF register based on registers A and F" do
-      cpu = CPU.new
+    context 'when the flag is on' do
+      let(:options) {{ f: 0b0100_0000 }}
 
-      cpu.af = 0xABCD
-
-      cpu.a.should == 0xAB
-      cpu.f.should == 0xCD
-    end
-
-    it "should get the 'virtual' BC register based on registers B and C" do
-      cpu = CPU.new(b: 0xAB, c: 0xCD)
-
-      cpu.bc.should == 0xABCD
-    end
-
-    it "should set the 'virtual' BC register based on registers C and B" do
-      cpu = CPU.new
-
-      cpu.bc = 0xABCD
-
-      cpu.b.should == 0xAB
-      cpu.c.should == 0xCD
-    end
-
-    it "should get the 'virtual' DE register based on registers D and E" do
-      cpu = CPU.new(d: 0xAB, e: 0xCD)
-
-      cpu.de.should == 0xABCD
-    end
-
-    it "should set the 'virtual' DE register based on registers D and E" do
-      cpu = CPU.new
-
-      cpu.de = 0xABCD
-
-      cpu.d.should == 0xAB
-      cpu.e.should == 0xCD
-    end
-
-    it "should get the 'virtual' HL register based on registers H and L" do
-      cpu = CPU.new(h: 0xAB, l: 0xCD)
-
-      cpu.hl.should == 0xABCD
-    end
-
-    it "should set the 'virtual' HL register based on registers H and L" do
-      cpu = CPU.new
-
-      cpu.hl = 0xABCD
-
-      cpu.h.should == 0xAB
-      cpu.l.should == 0xCD
+      its(:n_flag) { should be_true }
     end
   end
 
-  describe "Flags from F register" do
-    describe "Z flag" do
-      it "should be false when its off" do
-        cpu = CPU.new(f: 0b0111_0000)
+  describe '#h_flag' do
+    context 'when the flag is off' do
+      let(:options) {{ f: 0b1101_0000 }}
 
-        cpu.z_flag.should be_false
-      end
-
-      it "should be true when its on" do
-        cpu = CPU.new(f: 0b1000_0000)
-
-        cpu.z_flag.should be_true
-      end
+      its(:h_flag) { should be_false }
     end
 
-    describe "N flag" do
-      it "should be false when its off" do
-        cpu = CPU.new(f: 0b1011_0000)
+    context 'when the flag is on' do
+      let(:options) {{ f: 0b0010_0000 }}
 
-        cpu.n_flag.should be_false
-      end
-
-      it "should be true when its on" do
-        cpu = CPU.new(f: 0b0100_0000)
-
-        cpu.n_flag.should be_true
-      end
-    end
-
-    describe "H flag" do
-      it "should be false when its off" do
-        cpu = CPU.new(f: 0b1101_0000)
-
-        cpu.h_flag.should be_false
-      end
-
-      it "should be true when its on" do
-        cpu = CPU.new(f: 0b0010_0000)
-
-        cpu.h_flag.should be_true
-      end
-    end
-
-    describe "C flag" do
-      it "should be false when its off" do
-        cpu = CPU.new(f: 0b1110_0000)
-
-        cpu.c_flag.should be_false
-      end
-
-      it "should be true when its on" do
-        cpu = CPU.new(f: 0b0001_0000)
-
-        cpu.c_flag.should be_true
-      end
+      its(:h_flag) { should be_true }
     end
   end
 
-  it "should be able to read the next byte from memory and increment the program counter" do
-    cpu = CPU.new
+  describe '#c_flag' do
+    context 'when the flag is off' do
+      let(:options) {{ f: 0b1110_0000 }}
 
-    cpu.load_with(0xAB)
+      its(:c_flag) { should be_false }
+    end
 
-    cpu.next_byte.should == 0xAB
-    cpu.pc.should == 1
+    context 'when the flag is on' do
+      let(:options) {{ f: 0b0001_0000 }}
+
+      its(:c_flag) { should be_true }
+    end
   end
 
-  it "should be able to read the next word from memory and increment the program counter" do
-    cpu = CPU.new
+  context 'when a program is loaded' do
+    before { cpu.load_with(0xAB, 0xCD) }
 
-    cpu.load_with(0xAB, 0xCD)
+    describe '#next_byte' do
+      let!(:result) { cpu.next_byte }
 
-    cpu.next_word.should == 0xABCD
-    cpu.pc.should == 2
+      specify { result.should == 0xAB }
+      its(:pc) { should == 1 }
+    end
+
+    describe '#next_word' do
+      let!(:result) { cpu.next_word }
+
+      specify { result.should == 0xABCD }
+      its(:pc) { should == 2 }
+    end
   end
 end
